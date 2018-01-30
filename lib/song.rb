3c107d57656263f2734c98bd4af879fc340bbfc6
Katrina Brinson <kbrinso@gmail.com>
2018-01-30 22:37:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,10 @@ class Song
   @@artist_count = 0
   @@genre_count = 0
   
-  def initialize
-
+  def initialize(song_name,song_artist,song_genre)
+    @song = song_name
+    @artist = song_artist
+    @genre = song_genre
   end
   
   def self.artists
