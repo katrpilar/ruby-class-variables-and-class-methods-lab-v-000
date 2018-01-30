5d209c39f861e79fb0bb54e14f7fad16788c41d8
Katrina Brinson <kbrinso@gmail.com>
2018-01-30 22:36:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,16 @@
 class Song 
   @@song_count = 0
   @@artist_count = 0
+  @@genre_count = 0
   
   def initialize
-    
+
+  end
+  
+  def self.artists
+    @@artist_count
+  end
+  
+  def self.genre_count
   end
 end
