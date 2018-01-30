d23b0c118d6b2c8e29c4147c54a0477a2708b536
Katrina Brinson <kbrinso@gmail.com>
2018-01-30 22:43:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,21 @@ class Song
   @@song_count = 0
   @@artist_count = 0
   @@genre_count = 0
+  @@all_artists = []
+  @@num_of_song_instances
+  @@all_genres = []
   
   def initialize(song_name,song_artist,song_genre)
     @song = song_name
     @artist = song_artist
     @genre = song_genre
+    @@all_artists << @artist
+    @@num_of_song_instances += 1
+    @@all_genres <<
+  end
+  
+  def self.count 
+    @@num_of_song_instances
   end
   
   def self.artists
